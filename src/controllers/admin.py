94e57e3930361c5e4ab663a112d1ad68f8b26ae9
Nathan Alison <nathan.f.alison@gmail.com>
2013-04-29 00:27:15 -0500

    Move the voters handler to the controller.
    -Delete voters.py

--- src/controllers/admin.py
+++ src/controllers/admin.py
@@ -7,16 +7,18 @@ import logging
 import webapp2
 
 from authentication import auth
-from models import models
+from models import models, tasks
 from models.webapputils import render_template, json_response
+from models.admin_.organization_.election import get_panel
 
+VOTER_PAGE_URL = '/admin/organization/election/voters'
+VOTER_TASK_URL = '/tasks/admin/organization/election/voters'
 MSG_NOT_AUTHORIZED = ('We\'re sorry, you\'re not an organization administrator. Please contact the website administration '
                      'if you are interested in conducting elections for your organization.')
 
 from models.admin_.organization_.election import ElectionPanelHandler
 from models.admin_.organization_.election_.information import ElectionInformationHandler
 from models.admin_.organization_.election_.positions import ElectionPositionsHandler
-from models.admin_.organization_.election_.voters import ElectionVotersHandler
 
 class OrganizationPanelHandler(webapp2.RequestHandler):
 
@@ -78,6 +80,79 @@ class OrganizationPanelHandler(webapp2.RequestHandler):
             setattr(org, field, data[field].strip())
         org.put()
         return json_response('OK', 'Updated')
+
+class ElectionVotersHandler(webapp2.RequestHandler):
+
+    def get(self):
+        # Authenticate user
+        voter = auth.get_voter(self)
+        status = models.get_admin_status(voter)
+        if not status:
+            return render_template('/templates/message',
+                {'status': 'Not Authorized', 'msg': MSG_NOT_AUTHORIZED})
+
+        # Get election
+        election = auth.get_election()
+        if not election:
+            return get_panel(
+                VOTER_PAGE_URL,
+                {'status': 'ERROR','msg': 'No election found.'},
+                None)
+
+        if election.universal:
+            return get_panel(
+                VOTER_PAGE_URL,
+                {'status': 'Universal Election',
+                 'msg': 'This is a universal election, anyone with a valid '
+                        'NetID can vote for. Therefore you cannot manage '
+                        'the voters list.'},
+                None)
+
+        data = {'status': 'OK',
+                'id': str(election.key()),
+                'voters': sorted(list(models.get_voter_set(election)))}
+        logging.info(data)
+        return get_panel(VOTER_PAGE_URL, data, data.get('id'))
+
+    def post(self):
+        methods = {
+            'add_voters': self.add_voters,
+            'delete_voters': self.delete_voters
+        }
+
+        # Get election
+        election = auth.get_election()
+        if not election:
+            return
+
+        # Get the method
+        data = json.loads(self.request.get('data'))
+        method = data['method']
+        logging.info('Method: %s\n Data: %s', method, data)
+        if method in methods:
+            methods[method](election, data)
+        else:
+            return json_response('ERROR', 'Unknown method')
+
+    def add_voters(self, election, data):
+        tasks.voters_task(election, data, 'add_voters', VOTER_TASK_URL)
+        voter_set = models.get_voter_set(election)
+        for voter in data['voters']:
+            voter_set.add(voter)
+        out = {'status': 'OK',
+               'msg': 'Adding',
+               'voters': sorted(list(voter_set))}
+        self.response.write(json.dumps(out))
+
+    def delete_voters(self, election, data):
+        tasks.voters_task(election, data, 'delete_voters', VOTER_TASK_URL)
+        voter_set = models.get_voter_set(election)
+        for voter in data['voters']:
+            voter_set.discard(voter)
+        out = {'status': 'OK',
+               'msg': 'Adding',
+               'voters': sorted(list(voter_set))}
+        self.response.write(json.dumps(out))
 
 app = webapp2.WSGIApplication([
     ('/admin/organization', OrganizationPanelHandler),
