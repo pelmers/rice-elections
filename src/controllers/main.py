c0b963832cd0952b370b836127604b2b2f7770ed
Nathan Alison <nathan.f.alison@gmail.com>
2013-04-29 01:33:57 -0500

    Convert main.py controller to microapp.

--- src/controllers/main.py
+++ src/controllers/main.py
@@ -10,26 +10,22 @@ import json
 import webapp2
 
 from models import models
+import utils
 from utils import render_template, json_response
 
-class IndexHandler(webapp2.RequestHandler):
-    """Handles the main page."""
-    def get(self):
-        return render_template('/home', {})
+app = utils.App(debug=True)
 
-class StaticHandler(webapp2.RequestHandler):
-    """Handles GET requests for static pages."""
-    def get(self):
-        return render_template(self.request.path, {})
+@app.route('/')
+def index(request):
+    """Handle the main page."""
+    return render_template('/home', {})
 
-class VotesCountHandler(webapp2.RequestHandler):
-    def get(self):
-        votes_count = models.get_vote_count()
-        return webapp2.Response(json.dumps({'votes_count': votes_count}))
+@app.route('/.*')
+def static_page(request):
+    """Handle GET requests for static pages."""
+    return render_template(request.path, {})
 
-
-app = webapp2.WSGIApplication([
-    ('/', IndexHandler),
-    ('/stats/votes-count', VotesCountHandler),
-    ('/.*', StaticHandler)
-], debug=True)
+@app.route('/stats/votes-count')
+def votes_count(request):
+    votes_count = models.get_vote_count()
+    return json.dumps({'votes_count': votes_count})
