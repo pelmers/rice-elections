130e44e6a1f7fd8c2a67f544cf6ba1a4ca89f5bf
Nathan Alison <nathan.f.alison@gmail.com>
2013-04-29 00:37:09 -0500

    Move the information.py handler to controller.

--- src/controllers/admin.py
+++ src/controllers/admin.py
@@ -7,10 +7,12 @@ import logging
 import webapp2
 
 from authentication import auth
+from datetime import datetime
 from models import models, tasks
 from models.webapputils import render_template, json_response
 from models.admin_.organization_.election import get_panel
 
+INFO_TASK_URL = '/tasks/admin/organization/election/information'
 VOTER_TASK_URL = '/tasks/admin/organization/election/voters'
 MSG_NOT_AUTHORIZED = ('We\'re sorry, you\'re not an organization administrator. Please contact the website administration '
                      'if you are interested in conducting elections for your organization.')
@@ -78,6 +80,83 @@ class OrganizationPanelHandler(webapp2.RequestHandler):
             setattr(org, field, data[field].strip())
         org.put()
         return json_response('OK', 'Updated')
+
+class ElectionInformationHandler(webapp2.RequestHandler):
+
+    def get(self):
+        # Authenticate user
+        voter = auth.get_voter(self)
+        status = models.get_admin_status(voter)
+        if not status:
+            return render_template('/templates/message', 
+                {'status': 'Error', 'msg': 'Not Authorized'})
+        
+        data = {}
+
+        # Get election
+        election = auth.get_election()
+        page_url = '/admin/organization/election/information'
+        if election:
+            data = {'id': str(election.key()),
+                    'election': election.to_json()}
+        return get_panel(page_url, data, data.get('id'))
+
+    def post(self):
+        methods = {
+            'get_election': self.get_election,
+            'update_election': self.update_election
+        }
+
+        # Authenticate user
+        org = auth.get_organization()
+        if not org:
+            return json_response('ERROR', 'Not Authorized')
+
+        # Get election
+        election = auth.get_election()
+
+        # Get the method
+        data = json.loads(self.request.get('data'))
+        method = data['method']
+        logging.info('Method: %s\n Data: %s', method, data)
+        if method in methods:
+            methods[method](election, data)
+        else:
+            return json_response('ERROR', 'Unkown method')
+
+    def get_election(self, election, data):
+        out = {'status': 'OK'}
+        if election:
+            out['election'] = election.to_json()
+        self.response.write(json.dumps(out))
+
+    def update_election(self, election, data):
+        out = {'status': 'OK'}
+        if not election:
+            # User must be trying to create new election
+            election = models.Election(
+                name=data['name'],
+                start=datetime.fromtimestamp(data['times']['start']),
+                end=datetime.fromtimestamp(data['times']['end']),
+                organization=auth.get_organization(),
+                universal=data['universal'],
+                hidden=data['hidden'],
+                result_delay=data['result_delay'])
+            election.put()
+            out['msg'] = 'Created'
+            auth.set_election(election)
+        else:
+            election.name = data['name']
+            election.start = datetime.fromtimestamp(data['times']['start'])
+            election.end = datetime.fromtimestamp(data['times']['end'])
+            election.universal = data['universal']
+            election.hidden = data['hidden']
+            election.result_delay = data['result_delay']
+            election.put()
+            out['msg'] = 'Updated'
+        tasks.schedule_result_computation(election, INFO_TASK_URL)
+        out['election'] = election.to_json()
+        self.response.write(json.dumps(out))
 
 class ElectionPositionsHandler(webapp2.RequestHandler):
 
