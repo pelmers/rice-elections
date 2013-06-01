6de0d18ca13cd24137eef7605bc94b166b652d6f
Waseem Ahmad <waseem@rice.edu>
2013-06-01 13:39:54 -0700

    Added login urls for Google, Yahoo, AOL

--- src/controllers/main.py
+++ src/controllers/main.py
@@ -9,13 +9,24 @@ import logging
 import json
 import webapp2
 
+from google.appengine.api import users
 from models import models
 from utils import BasePageHandler
+
+providers = {
+    'signin_google'   : 'https://www.google.com/accounts/o8/id',
+    'signin_yahoo'    : 'yahoo.com',
+    'signin_aol'      : 'aol.com'
+}
 
 class IndexHandler(BasePageHandler):
     """Handles the main page."""
     def get(self):
-        return self.render_template('/home', {})
+        page_data = {}
+        for provider, url in providers.items():
+            page_data[provider] = users.create_login_url('/admin',
+                                                         federated_identity=url)
+        return self.render_template('/home', page_data)
 
 class StaticHandler(BasePageHandler):
     """Handles GET requests for static pages."""
